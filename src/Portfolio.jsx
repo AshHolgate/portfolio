3efe33152234e67bc48c6ff3e645e361cade013e
Ashton Holgate <ashton.holgate@gmail.com>
2018-07-11 17:06:38 +0100

    Added initial fixed background styling

---
+++ src/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Portfolio.scss';
+
+class Portfolio extends Component {
+	render() {
+		return (
+			<div className="Portfolio">
+				<div className="hero-background-container">
+					<div className="hero-background-gradient"/>
+					<div className="hero-background-image"/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Portfolio;
